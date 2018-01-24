85094221eef5c3602780842954c3dbc1f75c667e
Melissa Cadavid <mcadavid@qualit.tech>
2018-01-24 10:25:34 -0500

    crear proyecto web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+//Rutas
+import { routing, appRoutingProviders } from './app.routing';
+
+//Componentes
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home.component';
+import { ErrorComponent } from './components/error.component';
+import { ProductosListComponent } from './components/productos-list.component';
+import { ProductoAddComponent } from './components/producto-add.component';
+import { ProductoDetailComponent } from './components/producto-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ErrorComponent,
+    ProductosListComponent,
+    ProductoAddComponent,
+    ProductoDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
